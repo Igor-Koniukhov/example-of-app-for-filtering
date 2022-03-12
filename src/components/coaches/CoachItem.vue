0aa81a4231772fc7f25d2784a404003774c638fe
igor Koniukhov <ikoniukhov@gmail.com>
2022-03-12 14:06:27 +0200

    first commit

---
+++ src/components/coaches/CoachItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <li>
+        <h3>{{fullName}}</h3>
+        <h4>${{rate}}/hour</h4>
+        <div>
+            <span v-for="area in areas" :key="area">{{area}}</span>
+        </div>
+        <div class="actions">
+            <router-link :to="coachContactLink">Contact</router-link>
+            <router-link :to="coachDetailsLink">View Details</router-link>
+        </div>
+    </li>
+</template>
+
+<script>
+
+export default {
+   props: ['id', 'firstName', 'lastName', 'rate', 'areas'], 
+   computed:{
+       fullName(){
+           return this.firstName + ' ' + this.lastName;
+       },
+       coachContactLink(){
+           return this.$route.path +'/'+this.id+'/contact' 
+       },
+       coachDetailsLink(){
+           return '/coaches/'+this.id;
+       }
+   }
+}
+</script>
---
+++ src/pages/coaches/CoachDetail.vue
@@ -0,0 +1,4 @@
+<template>
+    <router-view></router-view>
+    <router-link to="/coaches/c1/contact">Contacts</router-link>
+</template>
