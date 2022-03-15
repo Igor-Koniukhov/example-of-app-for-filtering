08eb41649c77a572796212c6ac3b395a68c0272a
igor Koniukhov <ikoniukhov@gmail.com>
2022-03-15 17:05:30 +0200

    implemented fetching method for get coach data

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -25,4 +25,26 @@ export default {
       id: userId,
     });
   },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://vue-http-demo-3cdf4-default-rtdb.firebaseio.com/coaches/.json`
+    );
+    const responseData = await response.json();
+    if (!response.ok) {
+      //..
+    }
+    const coaches = [];
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourly,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+    context.commit('setCoaches', coaches);
+  },
 };
